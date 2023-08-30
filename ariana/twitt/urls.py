6f8dcc8ef04db98759ac4725d53f780e5f84f1a8
shaparak <moradi@bithome.ir>
2023-08-30 15:49:50 +0330

    add urls follow timeline and reply post

--- ariana/twitt/urls.py
+++ ariana/twitt/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView
+from . import views
 
+app_name = 'twitt'
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('follow/<int:pk>', views.FollowUser),
+    path('timeline/', views.TimeLineGetView),
+    path('reply/post/<int:pk>', views.ReplyPostGetView)
 
 ]
