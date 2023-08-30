bdffd587dbc0db49628bd72a8cbc0d5a8d394869
shaparak <moradi@bithome.ir>
2023-08-30 12:15:14 +0330

    add ReplyPost model to models.py

--- ariana/twitt/models.py
+++ ariana/twitt/models.py
@@ -13,8 +13,8 @@ class BaseModel(models.Model):
 
 class Post(BaseModel):
     user = models.ForeignKey(User, verbose_name=_("Owner"), on_delete=models.CASCADE)
-    title = models.CharField(_("Title"), max_length=64)
-    text = models.TextField(_("Text"), null=False)
+    title = models.CharField(_("Title"), max_length=64, null=False)
+    body = models.TextField(_("Body"), null=False)
 
     def __str__(self):
         return self.title
@@ -23,3 +23,18 @@ class Post(BaseModel):
         db_table = "post"
         verbose_name = _("post")
         ordering = ['-created']
+
+
+class ReplyPost(BaseModel):
+    user = models.ForeignKey(User, verbose_name=_("user"), on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name=_("Post"), on_delete=models.CASCADE)
+    reply = models.TextField(_("Reply"), null=False)
+    parent_reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.reply
+
+    class Meta:
+        db_table = "reply_post"
+        verbose_name = _("reply_post")
+        ordering = ['-reply']
