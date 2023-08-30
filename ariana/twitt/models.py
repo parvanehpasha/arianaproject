3438523e4058019f9b284b12df891c456201dd8f
shaparak <moradi@bithome.ir>
2023-08-30 12:14:01 +0330

    add Post model to models.py

--- ariana/twitt/models.py
+++ ariana/twitt/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -8,3 +9,17 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Post(BaseModel):
+    user = models.ForeignKey(User, verbose_name=_("Owner"), on_delete=models.CASCADE)
+    title = models.CharField(_("Title"), max_length=64)
+    text = models.TextField(_("Text"), null=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "post"
+        verbose_name = _("post")
+        ordering = ['-created']
