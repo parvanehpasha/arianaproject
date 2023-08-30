a50a68f427c0db468f7fcaebbc4be55068d790e9
shaparak <moradi@bithome.ir>
2023-08-30 12:12:41 +0330

    add BaseModel to models.py

--- ariana/twitt/models.py
+++ ariana/twitt/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(_("Create"), auto_now_add=True)
+    updated = models.DateTimeField(_("Update"), auto_now=True)
+
+    class Meta:
+        abstract = True
