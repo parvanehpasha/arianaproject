a714eb4b0641a05e32d98b221b7b279575bfc7bd
shaparak <moradi@bithome.ir>
2023-08-30 13:00:09 +0330

    add admin register to twitt app

--- ariana/twitt/admin.py
+++ ariana/twitt/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, ReplyPost, LikePost, FollowUser
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'body']
+
+
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'reply', 'parent_reply']
+
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'like']
+    list_filter = ['like']
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ['following', 'follower', 'follow']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(ReplyPost, ReplyAdmin)
+admin.site.register(LikePost, LikeAdmin)
+admin.site.register(FollowUser, FollowAdmin)
